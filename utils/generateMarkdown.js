665eb83ee981366e04523ff01c0f4e3456474590
Brandon Mellus <mellusbrandon@gmail.com>
2021-07-24 19:26:28 -0400

    Implement renderLicenseBadge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,15 @@
+const licenses = require("./licenses");
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const formattedLicense = formatLicenseInput(license);
+  if (licenses.hasOwnProperty(formattedLicense)) {
+    return licenses[formattedLicense].badge;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -15,6 +24,11 @@ function generateMarkdown(data) {
   return `# ${data.title}
 
 `;
+}
+
+// Returns a formatted string for license inputs
+function formatLicenseInput(license) {
+  return license.split(" ").join("-").toLowerCase();
 }
 
 module.exports = generateMarkdown;
---
+++ utils/licenses.js
@@ -0,0 +1,61 @@
+const license = {
+    "apache-2.0": {
+        badge: "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+    },
+    "bsd-3-clause": {
+        badge: "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+    },
+    "bsd-2-clause": {
+        badge: "[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)"
+    },
+    "attribution-4.0-international": {
+        badge: "[![License: CC BY 4.0](https://licensebuttons.net/l/by/4.0/80x15.png)](http://creativecommons.org/licenses/by/4.0/)"
+    },
+    "attribution-sharealike-4.0-international": {
+        badge: "[![License: CC BY-SA 4.0](https://licensebuttons.net/l/by-sa/4.0/80x15.png)](http://creativecommons.org/licenses/by-sa/4.0/)"
+    },
+    "attribution-noncommercial-4.0-international": {
+        badge: "[![License: CC BY-NC 4.0](https://licensebuttons.net/l/by-nc/4.0/80x15.png)](http://creativecommons.org/licenses/by-nc/4.0/)"
+    },
+    "attribution-noderivates-4.0-international": {
+        badge: "[![License: CC BY-ND 4.0](https://licensebuttons.net/l/by-nd/4.0/80x15.png)](http://creativecommons.org/licenses/by-nd/4.0/)"
+    },
+    "attribution-noncommercial-sharealike-4.0-international": {
+        badge: "[![License: CC BY-NC-SA 4.0](https://licensebuttons.net/l/by-nc-sa/4.0/80x15.png)](http://creativecommons.org/licenses/by-nc-sa/4.0/)"
+    },
+    "attribution-noncommerical-noderivatives-4.0-international": {
+        badge: "[![License: CC BY-NC-ND 4.0](https://licensebuttons.net/l/by-nc-nd/4.0/80x15.png)](http://creativecommons.org/licenses/by-nc-nd/4.0/)"
+    },
+    "eclipse": {
+        badge: "[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)"
+    },
+    "gnu-gpl-v3": {
+        badge: "[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)"
+    },
+    "gnu-gpl-v2": {
+        badge: "[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://img.shields.io/badge/License-GPL%20v2-blue.svg)"
+    },
+    "gnu-agpl-v3": {
+        badge: "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)"
+    },
+    "gnu-lgpl-v3": {
+        badge: "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](http://www.gnu.org/licenses/lgpl-3.0)"
+    },
+    "gnu-fdl-v1.3": {
+        badge: "[![License: FDL 1.3](https://img.shields.io/badge/License-FDL%20v1.3-blue.svg)](http://www.gnu.org/licenses/fdl-1.3)"
+    },
+    "ibm": {
+        badge: "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)"
+    },
+    "mit": {
+        badge: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+    },
+    "mozilla": {
+        badge: "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+    },
+    "zlib": {
+        badge: "[![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)](https://opensource.org/licenses/Zlib)"
+    }
+};
+
+module.exports = license;
